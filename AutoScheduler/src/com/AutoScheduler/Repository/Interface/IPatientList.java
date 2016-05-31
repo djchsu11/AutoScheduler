8807e5da35da3cacab96c22d41ae1c9d238ca46f
djchsu11 <or.no.world@gmail.com>
2016-05-31 18:57:22 -0500

    Fleshed out interfaces for patient and therapist lists in the Repository. Next Steps: Create concrete classes for PatientList and TherapistList

--- AutoScheduler/src/com/AutoScheduler/Repository/Interface/IPatientList.java
+++ AutoScheduler/src/com/AutoScheduler/Repository/Interface/IPatientList.java
@@ -1,5 +1,13 @@
 package com.AutoScheduler.Repository.Interface;
 
+import com.AutoScheduler.Domain.*;
+import java.util.List;
+import java.util.ArrayList;
+
 public interface IPatientList {
+	
+	List<Patient> patientList=new ArrayList<Patient>();
+	
+	public List<Patient> getListOfPatients();
 
 }
--- AutoScheduler/src/com/AutoScheduler/Repository/Interface/ITherapistList.java
+++ AutoScheduler/src/com/AutoScheduler/Repository/Interface/ITherapistList.java
@@ -1,5 +1,11 @@
 package com.AutoScheduler.Repository.Interface;
 
-public interface ITherapistList {
+import com.AutoScheduler.Domain.*;
+import java.util.List;
+import java.util.ArrayList;
 
+public interface ITherapistList {
+	List <Therapist> therapistList = new ArrayList<Therapist>();
+	
+	public List<Therapist> getListOfTherapists();
 }
